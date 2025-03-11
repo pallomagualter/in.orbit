bfc3936caa2c90e1fb826afd3261585b96556bda
pallomagualter <pallomakcgualter@gmail.com>
2025-03-11 17:13:20 -0300

    feat: included list pending goals by week and function to goal completion

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,16 +1,24 @@
 import fastify from 'fastify'
+import z from 'zod'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { createGoal } from '../functions/createGoal'
-import z from 'zod'
+import { getWeekPendingGoals } from '../functions/getWeekPendingGoals'
+import { createGoalCompletion } from '../functions/createGoalCompletion'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return { pendingGoals }
+})
 
 app.post(
   '/goals',
@@ -28,6 +36,24 @@ app.post(
     await createGoal({
       title,
       desiredWeeklyFrequency,
+    })
+  }
+)
+
+app.post(
+  '/completions',
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async request => {
+    const { goalId } = request.body
+
+    await createGoalCompletion({
+      goalId,
     })
   }
 )
